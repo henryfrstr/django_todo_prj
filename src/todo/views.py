fb0ee39f9dd8bf516417940b47cdde01562e11c1
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-27 20:36:50 +0300

    added Todo Modeş

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 def home(request):
     return render(request, "todo/home.html")
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
